bfa93f33d35bb515768f6e6fbd107e80074e9899
MicaSeba <micaela.a.sebastian@gmail.com>
2023-02-24 16:22:25 -0500

    delete and put funtion updates

--- tour-app/src/components/Delete.js
+++ tour-app/src/components/Delete.js
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import { useState } from 'react';
 
+
 function Delete() {    
     
     //added use state hooks to keep track of user inputs
@@ -8,9 +9,38 @@ function Delete() {
 
     let sendCall = () => {
         console.log("Delete button clicked!"); //debuging
-        console.log("given tour id:", tid); //debuging     
+        console.log("given tour id:", tid); //debuging 
+        let getResponse
+        getResponse = getByTourId(tid);    
     }
 
+    async function getByTourId(id) {
+        console.log("inside button DELETE trigger");
+        let url = "https://2d7tkc5pj2.execute-api.us-east-1.amazonaws.com/beta/tours/delete?id="+id;
+        console.log("id: ", id);
+        console.log("url: ", url);
+        if (!id) {
+            console.log("DELETE parameters incomplete. Canceling request.");
+            window.alert("DELETE parameters incomplete. Canceling request.");
+            return;
+        }
+        
+        fetch(url, 
+            {
+                method: 'DELETE'
+            })
+            .then((response) => response.json().then((data) => {
+                console.log("response:", response);
+                console.log("data:", data);
+                window.alert(data.message);
+                //document.getElementById("del_res").innerHTML = data.message;
+            }))
+            .catch((error) => {
+                console.log("error:", error);
+                window.alert(error);
+            })
+    }
+    
     return(
         <div className="container">
             <h1>Delete Page</h1>
@@ -21,7 +51,7 @@ function Delete() {
                                           
                 </div>
                 <div>
-                    <Button text='Delete' onClick={sendCall()}/>
+                    <Button text='Delete' onClick= {()=> sendCall()}/>
                 </div>
             </div>
         </div>
