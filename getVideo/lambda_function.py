fef3d80cb6815b168f120a042db9013dd956dfa1
ThomasDMoraes <tmoraes2020@fau.edu>
2023-05-11 18:06:13 -0300

    updated lambda code (final version before end of semester)

--- getVideo/lambda_function.py
+++ getVideo/lambda_function.py
@@ -69,18 +69,41 @@ def lambda_handler(event, context):
         #UUID used to make a unique primary key id
         keyDict = {'id' : str(uuid.uuid4())} 
         reqData.update(keyDict)
+        
+        #could add an if-else statement for image maps.
+        x = format(reqData.pop('x-coordinate'), ".15g")
+        z = format(reqData.pop('z-coordinate'), ".15g")
+        reqData.update({"X": x})
+        reqData.update({"Z": z})
+        
         #for file upload (need to pass a 'key' parameter for file name when posting to s3)
         #possibly check on s3 if the file was uploaded (future code update)
         #also add try/except statements to send meaningful messages / status codes on error for dynamo and s3
         if(reqData.get("key", False)):
             fileKey = str(reqData.pop("key"))
-            tourUrl = {'url' : "https://tourify-tours.s3.amazonaws.com/" + fileKey}
-            reqData.update(tourUrl)
-        
-        #adding the item to the table
-        table.put_item(Item = reqData)
-        
-        message = {'message': 'Your tour has been uploaded!'} #make one for error also based on status code later
+            #checking for duplicates in DynamoDB (S3 automatically discards duplicate attempts)
+            duplicateSearch = table.scan(
+                FilterExpression='#fileName = :fileValue',
+                ExpressionAttributeValues={ ':fileValue' : fileKey},
+                ExpressionAttributeNames={ '#fileName': 'fileName'}
+            )
+            if(duplicateSearch['Count'] > 0): #duplicats found
+                message = {'message': 'Error: A tour for this file already exists!'}
+                statusCode = 400
+            else: #duplicates not found
+                tourUrl = {'url' : "https://tourify-tours.s3.amazonaws.com/" + fileKey}
+                fileName = {'fileName' : fileKey} #object key is needed for some S3 operations
+                reqData.update(tourUrl)
+                reqData.update(fileName)
+                #adding the item to the table
+                table.put_item(Item = reqData)
+                message = {'message': 'Your tour has been uploaded!'}
+                statusCode = 200
+                
+        else: #simplify code once s3 is fully deployed on reactjs. right now there are too many branches.
+            table.put_item(Item = reqData)
+            message = {'message': 'Your tour has been uploaded (without file upload)!'}
+            
         #response sent back to the client
         response = {
             'headers': {
@@ -88,44 +111,71 @@ def lambda_handler(event, context):
                 'Access-Control-Allow-Methods': 'OPTIONS,POST',
                 'Access-Control-Allow-Origin': '*'
             },
-            'statusCode' : 200,
+            'statusCode' : statusCode,
             'body' : json.dumps(message)
         }
         
+    #update (PUT) API endpoint
     elif (path == '/tours/update'):
         #retrieving request data, converting from JSON to a python dictionary
         reqData = json.loads(event['body'])
-        #separating the id key into a separate dictionary for the put_item function later
-        key = {"id": str(reqData["id"])} #right key format
-        #removing it from dictionary for consistency
-        reqData.pop('id')
-        #making a new dictionary with tuples holding attributes and values
-        items = reqData.items()
-        #put_item parameters initialized and filled in the FOR loop
-        expStr = 'SET '
-        attributeValues = {}
-        attributeNames = {}
-        i = 1
-        for item in items:
-            if (i > 1):
-                expStr += " , "
-            expStr += '#name'+str(i) + ' = ' + ':value'+str(i)
-            attributeNames.update({'#name'+str(i) : item[0]})
-            attributeValues.update({':value'+str(i) : item[1]})
-            i += 1
         
-        #print(expStr)    
-        #print(attributeValues)
-        #print(attributeNames)
-        
-        #updates the item with corresponding key using the given attributes. Can add parameters to return the modified item.
-        table.update_item(Key = key,
-            UpdateExpression = expStr,
-            ExpressionAttributeValues = attributeValues,
-            ExpressionAttributeNames = attributeNames
+        #scan for our item to update. find its file key. run duplicate search.
+        fileKey = str(reqData["fileName"])
+        duplicateSearch = table.scan(
+            FilterExpression='#fileName = :fileValue',
+            ExpressionAttributeValues={ ':fileValue' : fileKey},
+            ExpressionAttributeNames={ '#fileName': 'fileName'}
         )
+        print(duplicateSearch)
         
-        message = {'message': 'Your tour has been updated!'} #make one for error also based on status code later
+        if(duplicateSearch['Count'] > 0): #duplicates found
+            message = {'message': 'Error: A tour already has this file name.'}
+            statusCode = 400
+            #add an elif statement for marker collisions!
+        else:
+            #separating the id key into a separate dictionary for the put_item function later
+            key = {"id": str(reqData["id"])} #right key format
+            
+            #deleting current s3 file (upload needs to be from outside lambda later in frontend because limit is 5mb here)
+            currentRecord = table.get_item(Key = {'id' : key['id']})
+            s3.Object('tourify-tours', 'public/'+currentRecord['Item']['fileName']).delete()
+            
+            #removing it from dictionary for consistency
+            reqData.pop('id')
+            reqData.update({'url' : "https://tourify-tours.s3.amazonaws.com/" + str(fileKey)})
+            
+            #if statement for image maps.
+            if(reqData.get("x-coordinate", False) != False and reqData.get("z-coordinate", False) != False):
+                x = format(reqData.pop('x-coordinate'), ".15g")
+                z = format(reqData.pop('z-coordinate'), ".15g")
+                reqData.update({"X": x})
+                reqData.update({"Z": z})
+            
+            #making a new dictionary with tuples holding attributes and values
+            items = reqData.items()
+            #put_item parameters initialized and filled in the FOR loop
+            expStr = 'SET '
+            attributeValues = {}
+            attributeNames = {}
+            i = 1
+            for item in items:
+                if (i > 1):
+                    expStr += " , "
+                expStr += '#name'+str(i) + ' = ' + ':value'+str(i)
+                attributeNames.update({'#name'+str(i) : item[0]})
+                attributeValues.update({':value'+str(i) : item[1]})
+                i += 1
+            
+            #updates the item with corresponding key using the given attributes. Can add parameters to return the modified item.
+            table.update_item(Key = key,
+                UpdateExpression = expStr,
+                ExpressionAttributeValues = attributeValues,
+                ExpressionAttributeNames = attributeNames
+            )
+            
+            message = {'message': 'Your tour has been updated!'} #make one for error also based on status code later
+            statusCode = 200
         #response sent back to the client
         response = {
             'headers': {
@@ -133,7 +183,7 @@ def lambda_handler(event, context):
                 'Access-Control-Allow-Methods': 'OPTIONS,PUT',
                 'Access-Control-Allow-Origin': '*'
             },
-            'statusCode' : 200,
+            'statusCode' : statusCode,
             'body' : json.dumps(message)
         }
         
@@ -148,10 +198,14 @@ def lambda_handler(event, context):
         print("delete result:" + str(findResult))
         try:
             print("item found: " + str(findResult['Item']))
+            #deleting record from DynamoDB table
             delResult = table.delete_item(
                 Key = {'id' : id }
             )
-        
+            #deleting file from S3
+            #print("file name: " + findResult['Item']['fileName']) #debug
+            obj = s3.Object('tourify-tours', 'public/'+findResult['Item']['fileName']).delete()
+            
             message = {'message': 'Tour id='+id+' has been deleted!'}
         except:
             message = {'message': 'Tour id='+id+' does not exist!'}
